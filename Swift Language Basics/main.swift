1a4339a21a499e1eb40bbdd3f873986c5d854d91
Keith <keith@mwc-070199.dhcp.missouri.edu>
2017-09-08 15:07:33 -0500

    Initial Commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Keith on 9/8/17.
+//  Copyright © 2017 Keith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
